aa56db9242564c3f438428a1b4be2702348740ad
Dell-User <Dell-User@DESKTOP-I2HH80B>
2023-10-10 09:57:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter a number  ");
+double num=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter an operator");
+string op=Console.ReadLine();
+
+Console.WriteLine("Enter the second number");
+double num2=Convert.ToDouble(Console.ReadLine());
+
+if (op == "+")
+{ Console.WriteLine(num + num2); }
+
+else if (op== "-")
+{ Console.WriteLine(num - num2); }
+else if (op== "*")
+{ Console.WriteLine(num*num2); }
+else if (op== "/")
+{ Console.WriteLine(num/num2); }
+else
+{ Console.WriteLine("Not an operator"); }
+
+Console.Read();
+
+
